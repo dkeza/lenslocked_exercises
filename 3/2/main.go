5534295e265b26b740063a2903737521c419247d
dkeza <dkezic@yahoo.com>
2018-10-28 08:35:25 +0100

    Add custom 404 page

--- 3/2/main.go
+++ 3/2/main.go
@@ -17,9 +17,15 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<p>Contact page</p>")
 }
 
+func NotFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<p>Nothing here :(</p>")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.NotFoundHandler = http.HandlerFunc(NotFound)
 	http.ListenAndServe(":3000", r)
 }
